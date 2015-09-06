6fb836769cd38bf77bf547f20c2d653241d3d1a5
renerubio <rrrm_2@hotmail.com>
2015-09-06 14:39:14 +0200

    new commit
    this commit

---
+++ js/app-parse.js
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (c) 2015, Parse, LLC. All rights reserved.
+ *
+ *  You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ *  use, copy, modify, and distribute this software in source code or binary
+ *  form for use in connection with the web services and APIs provided by Parse.
+ *
+ *  As with any software that integrates with the Parse platform, your use of
+ *  this software is subject to the Parse Terms of Service
+ *  [https://www.parse.com/about/terms]. This copyright notice shall be
+ *  included in all copies or substantial portions of the software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ *
+ */
+
+// Insert your app's keys here:
+
+
+var PARSE_APPLICATION_ID = "gsTLTeeb66dNrTiU2MJhugt1dMol5Ik0EPBbaWse";
+var PARSE_JAVASCRIPT_KEY = "tHZwaVlaM6wE6ertnwSBAPYXCh5SrjpVWlxyMKf6";
+Parse.initialize( PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY );
+
+/*	Create new row */
+/*var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});*/
+
+/*	List results	*/
