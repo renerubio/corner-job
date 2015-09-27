1ab15c977a2afb6b02c122e99285f70c085ddbdd
albert olive <albertolivecorbella@gmail.com>
2015-09-27 11:49:18 +0200

    Called parse API on componentDidMount and showed on main page and listOffers page

--- js/react/listOffers.js
+++ js/react/listOffers.js
@@ -1,17 +1,58 @@
 'use strict';
 
 var OfferList = React.createClass({
-  getInitialState: function() {    
+
+  componentDidMount: function() {
+
+    var offersArray = [];
+
+    var Offer = Parse.Object.extend('Oferta');
+
+    var getOffers = new Parse.Query(Offer);
+
+    getOffers.find({
+
+        success: function(offers) {
+
+          _.map(offers, function(offer) {
+
+            offersArray.push({
+              id: offer.id, 
+              title: offer.get('title'), 
+              description: offer.get('description'), 
+              category: offer.get('category'), 
+              date_publishing: moment(offer.get('date_publishing')).format('MMM D, YYYY'),
+              date_finishing: moment(offer.get('date_finishing')).format('MMM D, YYYY'), 
+              company: offer.get('company'), 
+              address: offer.get('address')
+            });
+
+          });
+
+          showListOffers();
+
+        },
+        error: function(error) {
+            alert('Error: ' + error.code + ' ' + error.message);
+        }
+    });
+  },
+
+  getInitialState: function() {
+
     return {
       data: offersArray, 
       title : 'Lista de ofertas disponibles'
     };
+
   },
+
   editRow: function(e) {
 
     showEditOffer( e.target.value );
 
   },
+
   render : function() {
       return(
         <div>
