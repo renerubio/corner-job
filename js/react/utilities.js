f6e7669ee2b6b1512c19779af5bde28295a52bac
albert olive <albertolivecorbella@gmail.com>
2015-09-27 11:06:28 +0200

    create reactHandle to display functions on windows load. Added to utilities function that was used twice

--- js/react/utilities.js
+++ js/react/utilities.js
@@ -1,3 +1,18 @@
 var categories = ['categoría 1', 'categoría 2', 'categoría 3'];
-React.render(<OfferList />, 
-    document.getElementById('list_offers'));
+
+var getFormData = function(e) {
+
+    var data = {
+
+      title:  e.target.form.title.value,
+      description:  e.target.form.description.value,
+      category:  e.target.form.category.value,
+      date_publishing: e.target.form.date_publishing.value,
+      date_finishing: e.target.form.date_finishing.value,
+      company: e.target.form.company.value,
+      address: e.target.form.address.value
+
+    }
+
+    return data
+}
